bad4b42321b1db6fccaf5bdd5e9d65ec705d5668
Kacper Trochimiak <trochimiak.kacper@gmail.com>
2018-06-12 07:52:28 +0200

    Disable menu mnemonic

--- src/main/java/com/trochimiakk/program/Main.java
+++ src/main/java/com/trochimiakk/program/Main.java
@@ -4,9 +4,11 @@ import com.google.inject.Guice;
 import com.google.inject.Injector;
 import com.trochimiakk.config.AppModule;
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyEvent;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -18,8 +20,13 @@ public class Main extends Application {
         fxmlLoader.setLocation(getClass().getResource("/views/main.fxml"));
         fxmlLoader.setControllerFactory(injector::getInstance);
         Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root, 800, 600);
+        scene.addEventHandler(KeyEvent.KEY_PRESSED, event -> {
+            if(event.isAltDown())
+                event.consume();
+        });
         primaryStage.setTitle("Flashcards");
-        primaryStage.setScene(new Scene(root, 800, 600));
+        primaryStage.setScene(scene);
         primaryStage.show();
     }
 
